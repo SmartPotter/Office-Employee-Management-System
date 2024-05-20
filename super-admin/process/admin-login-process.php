8e0b63c6cd8167ec2fa282a454bae0916dbc98d2
SmartPotter <medamaithri11@gmail.com>
2024-05-20 12:39:15 +0400

    yes

--- super-admin/process/admin-login-process.php
+++ super-admin/process/admin-login-process.php
@@ -9,7 +9,19 @@ if(isset($_POST['super_admin_username']) && ($_POST['super_admin_password'])){
 
   $super_admin_username = $_POST['super_admin_username'];
   $super_admin_password = $_POST['super_admin_password'];
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
 
+  // Create connection
+  $con = mysqli_connect($servername, $username, $password);
+
+  // Check connection
+  if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  //echo "Connected successfully";
+  mysqli_select_db($con, "ep");
   $admin_login_query = "SELECT * FROM super_admin_login WHERE username = '$super_admin_username' && password = '$super_admin_password' LIMIT 1";
   $admin_login = mysqli_query($con, $admin_login_query);
 
